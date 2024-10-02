76adf7edba00e930be2fd291acaad2c4413305d5
Miguel dos Santos Miranda <117553721+Miguelkkkk@users.noreply.github.com>
2024-10-01 22:29:08 -0300

    Selection screen
    Tela de seleção criada, animações funcionando, mas o código de seleção ainda não está pronto.

---
+++ DetentionProject/Assets/Scripts/SceneManager/SelectionScene/SelectionManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class SelectionsManager : MonoBehaviour
+{
+    public RectTransform imageRight;
+    public RectTransform imageLeft;
+    public CanvasGroup rightImageCanvasGroup;
+    public CanvasGroup leftImageCanvasGroup;
+    public float moveDistance = 200f;
+    public float transitionSpeed = 5f; 
+    public float fadeSpeed = 2f; 
+
+    private Vector3 rightImageStartPos;
+    private Vector3 leftImageStartPos;
+    private Vector2 targetPositionRight;
+    private Vector2 targetPositionLeft;
+
+    void Start()
+    {
+        rightImageStartPos = imageRight.anchoredPosition;
+        leftImageStartPos = imageLeft.anchoredPosition;
+
+        targetPositionRight = rightImageStartPos;
+        targetPositionLeft = leftImageStartPos;
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        if (mousePos.x > Screen.width / 2 + 100)
+        {
+            targetPositionRight = new Vector2(leftImageStartPos.x - moveDistance, leftImageStartPos.y);
+            targetPositionLeft = new Vector2(rightImageStartPos.x, rightImageStartPos.y);
+
+            rightImageCanvasGroup.alpha = Mathf.Lerp(rightImageCanvasGroup.alpha, 0.3f, Time.deltaTime * fadeSpeed);
+            leftImageCanvasGroup.alpha = Mathf.Lerp(leftImageCanvasGroup.alpha, 1f, Time.deltaTime * fadeSpeed);
+        }
+        else if (mousePos.x < Screen.width / 2 - 100)
+        {
+            targetPositionLeft = new Vector2(rightImageStartPos.x + moveDistance, rightImageStartPos.y);
+            targetPositionRight = new Vector2(leftImageStartPos.x, leftImageStartPos.y);
+
+            rightImageCanvasGroup.alpha = Mathf.Lerp(rightImageCanvasGroup.alpha, 1f, Time.deltaTime * fadeSpeed);
+            leftImageCanvasGroup.alpha = Mathf.Lerp(leftImageCanvasGroup.alpha, 0.3f, Time.deltaTime * fadeSpeed);
+        }
+        else
+        {
+            targetPositionLeft = new Vector2(rightImageStartPos.x + moveDistance, rightImageStartPos.y);
+            targetPositionRight = new Vector2(leftImageStartPos.x - moveDistance, leftImageStartPos.y);
+
+            rightImageCanvasGroup.alpha = Mathf.Lerp(rightImageCanvasGroup.alpha, 1f, Time.deltaTime * fadeSpeed);
+            leftImageCanvasGroup.alpha = Mathf.Lerp(leftImageCanvasGroup.alpha, 1f, Time.deltaTime * fadeSpeed);
+        }
+
+        imageRight.anchoredPosition = Vector2.Lerp(imageRight.anchoredPosition, targetPositionRight, Time.deltaTime * transitionSpeed);
+        imageLeft.anchoredPosition = Vector2.Lerp(imageLeft.anchoredPosition, targetPositionLeft, Time.deltaTime * transitionSpeed);
+    }
+}
